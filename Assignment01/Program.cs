f4dac98dcac7e728ad1e097ebc296b27765d2da2
jdavid342u <156380215+jdavid342u@users.noreply.github.com>
2024-02-14 09:06:53 -0700

    Add files via upload

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -40,11 +40,11 @@ namespace Assignment01
                         {
                             newFileLines[i] += $";{appliance.NumberOfDoors};{appliance.Height};{appliance.Width}";
                         }
-                        else if(appliance is Vacuum)
+                        else if (appliance is Vacuum)
                         {
                             newFileLines[i] += $";{appliance.Grade};{appliance.BatteryVoltage}";
                         }
-                        else if(appliance is Microwave)
+                        else if (appliance is Microwave)
                         {
                             newFileLines[i] += $";{appliance.Capacity};{appliance.RoomType}";
                         }
@@ -82,6 +82,135 @@ namespace Assignment01
                 }
 
                 Console.WriteLine(""); /* Bottom margin for each asking block */
+            }
+        }
+
+        static void DisplayAppliancesByType(List<Appliance> applianceList)
+        {
+            Console.WriteLine("Appliance Types");
+            Console.WriteLine("1 - Refrigerators");
+            Console.WriteLine("2 - Vacuums");
+            Console.WriteLine("3 - Microwaves");
+            Console.WriteLine("4 - Dishwashers");
+            Console.WriteLine("Enter type of appliance:");
+            string UserResponse;
+            bool continueAsking = true;
+
+            while (continueAsking)
+            {
+                UserResponse = Console.ReadLine().Trim();
+                if (UserResponse == "1")
+                {
+                    Console.WriteLine("Enter number of doors: 2 (double door), 3 (three doors) or 4 (four doors):");
+                    while (continueAsking)
+                    {
+                        UserResponse = Console.ReadLine().Trim();
+                        if (UserResponse != "2" && UserResponse != "3" && UserResponse != "4")
+                        {
+                            Console.WriteLine("Please enter a valid number of doors (2, 3, or 4):");
+                        }
+                        else
+                        {
+                            continueAsking = false;
+                            Console.WriteLine("Matching refrigerators:\n");  
+                            foreach (Appliance appliance in applianceList)
+                            {
+                                if(appliance is Refrigerator)
+                                {
+                                    if (appliance.NumberOfDoors == Int32.Parse(UserResponse))
+                                    {
+                                        Console.WriteLine(appliance.ToString());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (UserResponse == "2")
+                {
+                    Console.WriteLine("Enter battery voltage value. 18 V (low) or 24 V (high):");
+                    while (continueAsking)
+                    {
+                        UserResponse = Console.ReadLine().Trim();
+                        if (UserResponse != "18" && UserResponse != "24")
+                        {
+                            Console.WriteLine("Please enter a valid number of battery voltage value: 18 (Low V) or 24 (High V):");
+                        }
+                        else
+                        {
+                            continueAsking = false;
+                            Console.WriteLine("Matching vacuums:\n");
+                            foreach (Appliance appliance in applianceList)
+                            {
+                                if (appliance is Vacuum)
+                                {
+                                    if (appliance.BatteryVoltage == Int32.Parse(UserResponse))
+                                    {
+                                        Console.WriteLine(appliance.ToString());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (UserResponse == "3")
+                {
+                    Console.WriteLine("Room where the microwave will be installed: K (kitchen) or W (work site):");
+                    while (continueAsking)
+                    {
+                        UserResponse = Console.ReadLine().Trim().ToUpper();
+                        if (UserResponse != "K" && UserResponse != "W")
+                        {
+                            Console.WriteLine("Please enter a valid room where the microwave will be installed: K (kitchen) or W (work site):");
+                        }
+                        else
+                        {
+                            continueAsking = false;
+                            Console.WriteLine("Matching microwaves:\n");
+                            foreach (Appliance appliance in applianceList)
+                            {
+                                if (appliance is Microwave)
+                                {
+                                    if (appliance.RoomType == UserResponse)
+                                    {
+                                        Console.WriteLine(appliance.ToString());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (UserResponse == "4")
+                {
+                    Console.WriteLine("Enter the sound rating of the dishwasher: Qt (Quietest), Qr (Quieter), Qu(Quiet) or M (Moderate):");
+                    while (continueAsking)
+                    {
+                        UserResponse = Console.ReadLine().Trim().ToUpper();
+                        if (UserResponse != "QT" && UserResponse != "QR" && UserResponse != "QU" && UserResponse != "M")
+                        {
+                            Console.WriteLine("Please enter a valid sound rating of the dishwasher: Qt (Quietest), Qr (Quieter), Qu(Quiet) or M (Moderate):");
+                        }
+                        else
+                        {
+                            continueAsking = false;
+                            Console.WriteLine("Matching dishwashers:\n");
+                            foreach (Appliance appliance in applianceList)
+                            {
+                                if (appliance is Dishwasher)
+                                {
+                                    if (appliance.SoundRating == UserResponse)
+                                    {
+                                        Console.WriteLine(appliance.ToString());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. please enter a valid type of appliance (1, 2, 3, or 4):");
+                }
             }
         }
     }
